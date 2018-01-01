daa04a7013d4f37d6d94243f2a0368b9faec058e
esc4pe_re4lity <fiamma.pellicane@gmail.com>
2018-01-01 09:44:20 +0100

    Modifications des Managers pour corriger une erreur

--- model/PostManager.php
+++ model/PostManager.php
@@ -1,29 +1,30 @@
 <?php
-class PostManager extends DBFactory
+require('DBFactory.php');
+class PostManager
 {
     public static function addPost(Post $post){
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         $req=$db->prepare('INSERT INTO posts(title, content, creationDate) VALUES(:title, :content, NOW())');
         $q->execute(array('title'=>$post->getTitle(),'content'=>$post->getContent()));
     }
     public static function getPost(){
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         $q=$db->query('SELECT id, title, content, creationDate, updated, updatedDate FROM posts WHERE id='.$_GET['id']);
         return $q;
     }
     public static function getAllPosts(){
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         $req=$db->prepare('SELECT id, title, content, creationDate, updated, updatedDate FROM posts ORDER BY id DESC LIMIT 0, 5');
         $req->execute();
         return $req;
     }
     public static function updatePost(){
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         $req=$db->prepare('UPDATE posts SET title=:title, content=:content, updated=true, updated_date=NOW() WHERE id='.$_GET['id']);
         $req->execute(array('title'=>$_POST['title'],'content'=>$_POST['content']));
     }
     public static function deletePost(){
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         $q=$db->query('DELETE FROM posts WHERE id='.$_GET['id']);
     }
 }
--- model/UserManager.php
+++ model/UserManager.php
@@ -1,20 +1,21 @@
 <?php
-class UserManager extends DBFactory
+require('DBFactory.php');
+class UserManager
 {
    public static function addUser (User $user){
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         $req=$db->prepare('INSERT INTO users(pseudo, password, email, creationDate) VALUES(:pseudo, :password, :email, NOW())');
         $req->execute(array('pseudo'=>$user->getPseudo(),'password'->getPassword(),'email'=>$user->getEmail()));
     }
     public static function login(User $user){
         // on crée une instance de User pour crypter le password et pour vérifier les infos rentrées par l'user
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         $req=$db->prepare('SELECT id, pseudo, password, email, groupe FROM users WHERE pseudo=:pseudo AND password=:password');
         $req->execute(array('pseudo'=>$user->getPseudo(),'password'=>$user->getPassword()));
         return $req;
     }
     public static function getUser($data){
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         if(is_int($data)){
             $q=$db->query('SELECT id, pseudo, email, creationDate, groupe FROM users WHERE id='.$data);
         }else{
@@ -24,18 +25,18 @@ class UserManager extends DBFactory
         return $q;
     }
     public static function getUsers(){
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         $q=$db->query('SELECT id, pseudo, email, creationDate, groupe FROM users ORDER BY pseudo');
         $q->execute();
         return $q;
     }
     public static function updateUser(User $user){
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         $req=$db->prepare('UPDATE users SET pseudo=:pseudo, password=:password, email=:email');
         $req->execute(array('pseudo'=>$user->getPseudo(),'password'=>$user->getPassword(),'email'=>$user->getEmail()));
     }
     public static function deleteUser(){
-        $db = parent::loadDB();
+        $db = DBFactory::loadDB();
         $q=$db->query('DELETE FROM users WHERE id='.$_GET['id']);
     }
 }
