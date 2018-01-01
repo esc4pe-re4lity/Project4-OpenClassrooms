71e7d27270a342a32dc9b1532477aab1f054fc77
esc4pe_re4lity <fiamma.pellicane@gmail.com>
2018-01-01 09:53:08 +0100

    Création d'un template dans le dossier view

--- view/allPosts.php
+++ view/allPosts.php
@@ -1,32 +1,30 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title><?= $title ?></title>
-        <link rel="stylesheet" type="text/css" href="public/css/style.css" />
-    </head>
-        
-    <body>
-        <header>
-            <?= $header ?>
-        </header>
-        <section>
-            <?php
+<?php $title = 'Mon blog'; ?>
+
+<?php ob_start(); ?>
+<h1>BLOG</h1>
+<p>
+    <a href="index.php?action=login">Connexion</a><br/>
+    <a href="index.php?action=createAccount">Inscription</a>
+</p>
+<?php $header = ob_get_clean(); ?>
+
+<?php ob_start(); ?>
+<?php
 while ($post = $posts->fetch(PDO::FETCH_ASSOC))
 {
 ?>
-            <div class="posts">
-                <h2><?= htmlspecialchars(utf8_encode($post['title'])) ?></h2>
-                <p>
-                    <?= nl2br(htmlspecialchars(utf8_encode($post['content']))) ?>
-                    <br />
-                    <span class="date"><?= $post['date'] ?></span>
-                    <a href="index.php?action=post&amp;id=<?= $post['id'] ?>">Commentaires</a>
-                </p>
-            </div>
+    <div class="posts">
+        <h2><?= htmlspecialchars(utf8_encode($post['title'])) ?></h2>
+        <p>
+            <?= nl2br(htmlspecialchars(utf8_encode($post['content']))) ?>
+            <br />
+            <span class="date"><?= $post['creationDate'] ?></span>
+            <a href="index.php?action=post&amp;id=<?= $post['id'] ?>">Commentaires</a>
+        </p>
+    </div>
 <?php
 }
 ?>
-        </section>
-    </body>
-</html>
+<?php $section = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
---
+++ view/template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+        <link rel="stylesheet" type="text/css" href="public/css/style.css" />
+    </head>
+        
+    <body>
+        <header>
+            <?= $header ?>
+        </header>
+        <section>
+            <?= $section ?>
+        </section>
+    </body>
+</html>
