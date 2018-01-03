d432d75117efe4e91e6540a7de47a6073fed51b7
esc4pe_re4lity <fiamma.pellicane@gmail.com>
2018-01-03 12:52:04 +0100

    MAJ du CommentManager suite à des bugs

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -5,29 +5,28 @@ class CommentManager
     public static function addComment(Comment $comment){
         $db = DBFactory::loadDB();
         $req=$db->prepare('INSERT INTO comments(idPost, author, content, creationDate) VALUES(:idPost, :author, :content, NOW())');
-        $q->execute(array('idPost'=>$comment->getIdPost(),'author'=>$comment->getAuthor(),'content'=>$comment->getContent()));
+        $req->execute(array('idPost'=>$comment->getIdPost(),'author'=>$comment->getAuthor(),'content'=>$comment->getContent()));
     }
     public static function getComment(){           // PAS NECESSAIRE - A VOIR
         $db = DBFactory::loadDB();
-        $q=$db->query('SELECT id, idPost, author, content, creationDate FROM comments WHERE id='.$_GET['id']);
+        $q=$db->query('SELECT * FROM comments WHERE id='.$_GET['id']);
         return $q;
     }
     public static function getComments(){
         $db = DBFactory::loadDB();
-        $q=$db->query('SELECT id, idPost, author, content, creationDate FROM comments WHERE idPost='.$_GET['id'].'ORDER BY id DESC');
+        $q=$db->query('SELECT * FROM comments WHERE id_post='.$_GET['id'].' ORDER BY id DESC');
         return $q;
     }
     public static function getSignaledComments(){
         $db = DBFactory::loadDB();
-        $req=$db->prepare('SELECT id, idPosts, author, content, creationDate FROM comments WHERE signaled=true');
-        // faire une jonction entre ka table signaledComments et la table comments
+        $req=$db->prepare('SELECT * FROM comments WHERE signaled=true');
+        // faire une jonction entre la table signaledComments et la table comments
         return $req;
     }
     public static function getAllComments(){
         $db = DBFactory::loadDB();
-        $req=$db->prepare('SELECT id, idPost, author, content, creationDate FROM comments ORDER BY id DESC');
-        $req->execute();
-        return $req;
+        $q=$db->query('SELECT * FROM comments ORDER BY id DESC');
+        return $q;
     }
     public static function updateComment(){
         $db = DBFactory::loadDB();
