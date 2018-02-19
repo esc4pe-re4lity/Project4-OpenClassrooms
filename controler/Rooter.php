c0d54ed49252fb0d102f0baa90c8fa65848d1c58
esc4pe_re4lity <fiamma.pellicane@gmail.com>
2018-02-19 21:01:38 +0100

    Modification du dossier controler pour rendre le projet encore plus orienté objet en créant une classe Rooter qui va initialiser l'URL

---
+++ controler/Rooter.php
@@ -0,0 +1,397 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Rooter
+ *
+ * @author Fiamma
+ */
+
+require('Controler.php');
+function chargerClasse($classe){
+    require('model/'.$classe.'.php');
+}
+spl_autoload_register('chargerClasse');
+session_start();
+
+class Rooter {
+    
+    protected $user;
+    
+    public function setUser($user){
+        if(is_object($user)){
+            $this->user = $user;
+        }
+    }
+    public function getUser(){
+        return $this->user;
+    }
+    
+    public function __construct(){
+        $this->initURL();
+    }
+    
+    private function ifAdmin($fileName){
+        if(isset($_SESSION['user'])){
+            $this->setUser($_SESSION['user']);
+            if($this->getUser()->getAdmin() === 1){
+                $url = 'view/admin/'.$fileName.'.php';
+                return $url;
+            }else{
+                $url = 'view/user/'.$fileName.'.php';
+                return $url;
+            }
+        }else{
+            $url = 'view/'.$fileName.'.php';
+            return $url;
+        }
+    }
+    private function allPosts(){
+        if(isset($_GET['page'])){
+            $page = (int)$_GET['page'];
+            $paging = Controler::paging($page);
+            if(!empty($page)){
+                if($page <= $paging->getNumberOfPages()){
+                    $posts = Controler::getAllPosts($paging);
+                    $url = $this->ifAdmin('allPosts');
+                    $user = $this->getUser();
+                    require($url);
+                }else{
+                    throw new Exception('Le numéro de la page n\'existe pas');
+                }
+            }else{
+                throw new Exception('Le numéro de la page doit être déterminé');
+            }
+        }else{
+            $paging = Controler::paging(1);
+            $posts = Controler::getAllPosts($paging);
+            $url = $this->ifAdmin('allPosts');
+            $user = $this->getUser();
+            require($url);
+        }
+    }
+    private function post(){
+        if(isset($_GET['id'])){
+            $id = (int)$_GET['id'];
+            if(!empty($id)){
+                $post = Controler::getPost();
+                if($post->rowCount() == 0){
+                    throw new Exception('Ce post n\'existe pas');
+                }else{
+                    $comments = Controler::getComments();
+                    $url = $this->ifAdmin('post');
+                    $user = $this->getUser();
+                    require($url);
+                }
+            }else{
+                throw new Exception('Aucun identifiant de post a été sélectionné');
+            }
+        }else{
+            throw new Exception('L\'identifiant du post doit être déterminé');
+        }
+    }
+    private function addPost(){
+        if(isset($_SESSION['user'])){
+            $user = $_SESSION['user'];
+            if($user->getAdmin() == 1){
+                if(isset($_POST['title'])&&isset($_POST['content'])){
+                    if(!empty(trim($_POST['title']))&&!empty(trim($_POST['content']))){
+                        $postId = Controler::addPost();
+                        $action = 'addPost';
+                        header('Location: index.php?action=post&id='.$postId);
+                    }else{
+                        throw new Exception('Formulaire incomplet ou vide');
+                    }
+                }else{
+                    require('view/admin/addPost.php');
+                }
+            }else{
+                throw new Exception('Vous devez être Admin pour accéder au contenu de cette page');
+            }
+        }else{
+            throw new Exception('Vous devez vous identifier pour pouvoir accéder au contenu de cette page');
+        }
+    }
+    private function updatePost(){
+        if(isset($_GET['id'])){
+            $id = (int)$_GET['id'];
+            if(!empty($id)){
+                $result = Controler::idPostExists();
+                if($result->rowCount() == 0){
+                    throw new Exception('Ce post n\'existe pas');
+                }else{
+                    if(isset($_SESSION['user'])){
+                        $user = $_SESSION['user'];
+                        if($user->getAdmin() == 1){
+                            if(isset($_POST['title'])&&isset($_POST['content'])){
+                                if(!empty(trim($_POST['title']))&&!empty(trim($_POST['content']))){
+                                    Controler::updatePost();
+                                    $action = 'updatePost';
+                                    header('Location: index.php?action=post&id='.$id);
+                                }else{
+                                    throw new Exception('Formulaire incomplet ou vide');
+                                }
+                            }else{
+                                $post = Controler::getPost();
+                                require('view/admin/updatePost.php');
+                            }
+                        }else{
+                            throw new Exception('Vous devez être Admin pour accéder au contenu de cette page');
+                        }
+                    }else{
+                        throw new Exception('Vous devez vous identifier pour pouvoir accéder au contenu de cette page');
+                    }
+                }
+            }else{
+                throw new Exception('Aucun identifiant de post a été sélectionné');
+            }
+        }else{
+            throw new Exception('L\'identifiant du post doit être déterminé');
+        }
+    }
+    private function deletePost(){
+        if(isset($_GET['id'])){
+            $id = (int)$_GET['id'];
+            if(!empty($id)){
+                $result = Controler::idPostExists();
+                if($result->rowCount() == 0){
+                    throw new Exception('Ce post n\'existe pas');
+                }else{
+                    if(isset($_SESSION['user'])){
+                        $user = $_SESSION['user'];
+                        if($user->getAdmin() == 1){
+                            Controler::deletePost();
+                            $action = 'deletePost';
+                            header('Location: index.php');
+                        }else{
+                            throw new Exception('Vous devez être Admin pour accéder au contenu de cette page');
+                        }
+                    }else{
+                        throw new Exception('Vous devez vous identifier pour pouvoir accéder au contenu de cette page');
+                    }
+                }
+            }else{
+                throw new Exception('Aucun identifiant de post a été sélectionné');
+            }
+        }else{
+            throw new Exception('L\'identifiant du post doit être déterminé');
+        }
+    }
+    private function addComment(){
+        if(isset($_GET['idPost'])){
+            $idPost = (int)$_GET['idPost'];
+            if(!empty($idPost)){
+                if(isset($_POST['content'])){
+                    if(!empty(trim($_POST['content']))){
+                        Controler::addComment();
+                        $action = 'addComment';
+                        header('Location: index.php?action=post&id='.$idPost);
+                    }else{
+                        throw new Exception('Formulaire incomplet ou vide');
+                    }
+                }else{
+                    throw new Exception('Les données du formulaire n\'ont pas été transmises');
+                }
+            }else{
+                throw new Exception('Aucun identifiant de post a été sélectionné');
+            }
+        }else{
+            throw new Exception('L\'identifiant du post doit être déterminé');
+        }
+    }
+    private function deleteComment(){
+        if(isset($_GET['id'])){
+            $id = (int)$_GET['id'];
+            if(!empty($id)){
+                if(isset($_SESSION['user'])){
+                    $user = $_SESSION['user'];
+                    if($user->getAdmin() == 1){
+                        Controler::deleteComment();
+                        $action = 'deleteComment';
+                        header('Location: index.php');
+                    }else{
+                        throw new Exception('Vous devez être Admin pour accéder au contenu de cette page');
+                    }
+                }else{
+                    throw new Exception('Vous devez vous identifier pour pouvoir accéder au contenu de cette page');
+                }
+            }else{
+                throw new Exception('Aucun identifiant de commentaire a été sélectionné');
+            }
+        }else{
+            throw new Exception('L\'identifiant du commentaire doit être déterminé');
+        }
+    }
+    private function reportComment(){
+        if(isset($_GET['id'])&&isset($_GET['idPost'])){
+            $id = (int)$_GET['id'];
+            $idPost = (int)$_GET['idPost'];
+            if(!empty($id)&&!empty($idPost)){
+                if(isset($_SESSION['user'])){
+                    $user = $_SESSION['user'];
+                    if($user->getAdmin() == 1){
+                        throw new Exception('L\'admin n\'a pas besoin de signaler les commentaires, il peut les supprimer directement');
+                    }else{
+                        Controler::reportComment();
+                        $action = 'reportComment';
+                        header('Location: index.php?action=post&id='.$idPost);
+                    }
+                }else{
+                    throw new Exception('Vous devez vous identifier pour pouvoir signaler le commentaire');
+                }
+            }else{
+                throw new Exception('Aucun identifiant de post a été sélectionné');
+            }
+        }else{
+            throw new Exception('L\'identifiant du post doit être déterminé');
+        }
+    }
+    private function getReportedComments(){
+        if(isset($_SESSION['user'])){
+            $user = $_SESSION['user'];
+            if($user->getAdmin() == 1){
+                $comments = Controler::getReportedComments();
+                if($comments->rowCount() == 0){
+                    throw new Exception('Il n\'y a pas de commentaires signalés');
+                }else{
+                    require('view/admin/reportedComments.php');
+                }
+            }else{
+                throw new Exception('Vous devez être Admin pour accéder au contenu de cette page');
+            }
+        }else{
+            throw new Exception('Vous devez vous identifier pour pouvoir signaler le commentaire');
+        }
+    }
+    private function createAccount(){
+        if(isset($_POST['pseudo'])&&isset($_POST['password'])&&isset($_POST['email'])){
+            if(!empty(trim($_POST['pseudo']))&&!empty(trim($_POST['password']))&&!empty(trim($_POST['email']))){
+                $result = Controler::pseudoExists();
+                if($result->rowCount() == 0){
+                    $user = Controler::createAccount();
+                    $_SESSION['user'] = $user;
+                    Controler::sendEmail();
+                    $action = 'createAccount';
+                    header('Location: index.php');
+                }else{
+                    throw new Exception('Pseudo déjà utilisé');
+                }
+            }else{
+                throw new Exception('Formulaire incomplet ou vide');
+            }
+        }else{
+            require('view/createAccount.php');
+        }
+    }
+    private function updateUser(){
+        if(isset($_SESSION['user'])){
+            $user = $_SESSION['user'];
+            if(isset($_POST['pseudo'])&&isset($_POST['email'])){
+                if(!empty(trim($_POST['pseudo']))&&!empty(trim($_POST['email']))){
+                    $result = Controler::pseudoExists();
+                    var_dump($result);/*
+                    if($result->rowCount() == 0 && $result !== $user->getPseudo()){
+                        Controler::updateUser();
+                        $action = 'updateUser';
+                        header('Location: index.php');
+                    }else{
+                        throw new Exception('Pseudo déjà utilisé');
+                    }*/
+                }else{
+                    throw new Exception('Formulaire incomplet ou vide');
+                }
+            }else{
+                if($user->getAdmin() == 1){
+                    require('view/admin/updateUser.php');
+                }else{
+                    require('view/user/updateUser.php');
+                }
+            }
+        }else{
+            throw new Exception('Vous devez vous identifier pour pouvoir accéder au contenu de cette page');
+        }
+    }
+    private function login(){
+        if(isset($_POST['pseudo'])&&isset($_POST['password'])){
+            $result = Controler::login();
+            if(is_string($result)){
+                throw new Exception($result);
+            }else{
+                $_SESSION['user'] = $result;
+                $action = 'login';
+                header('Location: index.php');
+            }
+        }else{
+            require('view/login.php');
+        }
+    }
+    private function logout(){
+        $_SESSION=array();
+        session_destroy();
+        $action = 'logout';
+        header('Location: index.php');
+    }
+    private function initURL(){
+        try{
+            if(isset($_GET['action'])){
+                if($_GET['action'] == 'allPosts'){
+                    $this->allPosts();
+                }
+                elseif($_GET['action'] == 'post'){
+                    $this->post();
+                }
+                elseif($_GET['action'] == 'addPost'){
+                    $this->addPost();
+                }
+                elseif($_GET['action'] == 'updatePost'){
+                    $this->updatePost();
+                }
+                elseif($_GET['action'] == 'deletePost'){
+                    $this->deletePost();
+                }
+                elseif($_GET['action'] == 'addComment'){
+                    $this->addComment();
+                }
+                elseif($_GET['action'] == 'deleteComment'){
+                    $this->deleteComment();
+                }
+                elseif($_GET['action'] == 'reportComment'){
+                    $this->reportComment();
+                }
+                elseif($_GET['action'] == 'getReportedComments'){
+                    $this->getReportedComments();
+                }
+                elseif($_GET['action'] == 'createAccount'){
+                    $this->createAccount();
+                }
+                elseif($_GET['action'] == 'updateUser'){
+                    $this->updateUser();
+                }
+                elseif($_GET['action'] == 'login'){
+                    $this->login();
+                }
+                elseif($_GET['action'] == 'logout'){
+                    $this->logout();
+                }
+                else{
+                    throw new Exception('L\'action demandée ne correspond à aucune action répertoriée');
+                }
+            }else{
+                $paging = Controler::paging(1);
+                $posts = Controler::getAllPosts($paging);
+                $url = $this->ifAdmin('allPosts');
+                $user = $this->getUser();
+                require($url);
+            }
+        }catch(Exception $e){
+            $url = $this->ifAdmin('error');
+            $user = $this->getUser();
+            require($url);
+        }
+    }
+}
--- controler/controler.php
+++ controler/controler.php
@@ -1,19 +1,170 @@
 <?php
 class Controler
 {
-    public static function getAllPosts(){
-        require_once('../model/PostManager.php');
-        $posts = PostManager::getAllPosts();
+    public static function paging($page){
+        require_once('model/Paging.php');
+        require_once('model/PostManager.php');
+        $paging = new Paging();
+        $postManager = new PostManager();
+        $numberOfPosts = $postManager->countPosts();
+        $paging->setPage($page);
+        $paging->setLimit(5);
+        $paging->setOffset();
+        $paging->setNumberOfPosts($numberOfPosts);
+        $paging->setNumberOfPages();
+        return $paging;
+    }
+    public static function getAllPosts(Paging $paging){
+        require_once('model/PostManager.php');
+        $postManager = new PostManager();
+        $posts = $postManager->getAllPosts($paging);
         return $posts;
     }
     public static function getPost(){
-        require_once('../model/PostManager.php');
-        $post = PostManager::getPost();
+        require_once('model/PostManager.php');
+        $postManager = new PostManager();
+        $post = $postManager->getPost();
         return $post;
     }
+    public static function idPostExists(){
+        require_once('model/PostManager.php');
+        $postManager = new PostManager();
+        $result = $postManager->idPostExists();
+        return $result;
+    }
+    public static function addPost(){
+        require_once('model/Post.php');
+        require_once('model/PostManager.php');
+        $post = new Post();
+        $postManager = new PostManager();
+        $title = utf8_decode($_POST['title']);
+        $content = utf8_decode($_POST['content']);
+        $post->setTitle($title);
+        $post->setContent($content);
+        $post->setExcerpt();
+        $postId = $postManager->addPost($post);
+        return $postId;
+    }
+    public static function updatePost(){
+        require_once('model/Post.php');
+        require_once('model/PostManager.php');
+        $post = new Post();
+        $postManager = new PostManager();
+        $title = utf8_decode($_POST['title']);
+        $content = utf8_decode($_POST['content']);
+        $post->setTitle($title);
+        $post->setContent($content);
+        $post->setExcerpt();
+        $postManager->updatePost($post);
+    }
+    public static function deletePost(){
+        require_once('model/PostManager.php');
+        $postManager = new PostManager();
+        $postManager->deletePost();
+    }
     public static function getComments(){
-        require_once('../model/CommentManager.php');
-        $comments = CommentManager::getComments();
+        require_once('model/CommentManager.php');
+        $commentManager = new CommentManager();
+        $comments = $commentManager->getComments();
         return $comments;
+    }
+    public static function addComment(){
+        require_once('model/Comment.php');
+        require_once('model/CommentManager.php');
+        $comment = new Comment();
+        $commentManager = new CommentManager();
+        if(isset($_SESSION['user'])){
+            $user = $_SESSION['user'];
+            $comment->setAuthor($user->getPseudo());
+        }else{
+            $comment->setAuthor('anonyme');
+        }
+        $content = utf8_decode($_POST['content']);
+        $comment->setContent($content);
+        $comment->setIdPost((int)$_GET['idPost']);
+        $commentManager->addComment($comment);
+    }
+    public static function getReportedComments(){
+        require_once('model/ReportedCommentManager.php');
+        $reportedCommentManager = new ReportedCommentManager();
+        $reportedComments = $reportedCommentManager->getReportedComments();
+        return $reportedComments;
+    }
+    public static function reportComment(){
+        require_once('model/ReportedCommentManager.php');
+        $reportedCommentManager = new ReportedCommentManager();
+        $reportedCommentManager->reportComment();
+    }
+    public static function deleteComment(){
+        require_once('model/CommentManager.php');
+        require_once('model/ReportedCommentsManager.php');
+        $commentManager = new CommentManager();
+        $reportedCommentsManager = new ReportedCommentsManager();
+        $commentManager->deleteComment();
+        $reportedCommentsManager->deleteReportedComment();
+    }
+    public static function login(){
+        require_once('model/User.php');
+        require_once('model/UserManager.php');
+        $user = new User();
+        $userManager = new UserManager();
+        $pseudo = utf8_decode($_POST['pseudo']);
+        $password = utf8_decode($_POST['password']);
+        $user->setPseudo($pseudo);
+        $user->setPassword($password);
+        $result = $userManager->login($user);
+        $row=$result->fetch();
+        if($row['pseudo']==$user->getPseudo() && $row['password']==$user->getPassword()){
+            $user->setEmail($row['email']);
+            $user->setAdmin($row['admin']);
+            return $user;
+        }else{
+            return 'Le pseudo et/ou le mot de passe est incorrect';
+        }
+    }
+    public static function pseudoExists(){
+        require_once('model/User.php');
+        require_once('model/UserManager.php');
+        $user = new User();
+        $userManager = new UserManager();
+        $pseudo = utf8_decode($_POST['pseudo']);
+        $user->setPseudo($pseudo);
+        $result = $userManager->pseudoExists($user);
+        return $result;
+    }
+    public static function createAccount(){
+        require_once('model/User.php');
+        require_once('model/UserManager.php');
+        $user = new User();
+        $userManager = new UserManager();
+        $pseudo = utf8_decode($_POST['pseudo']);
+        $password = utf8_decode($_POST['password']);
+        $email = utf8_decode($_POST['email']);
+        $user->setPseudo($pseudo);
+        $user->setPassword($password);
+        $user->setEmail($email);
+        $user->setAdmin(false);
+        $userManager->addUser($user);
+        return $user;
+    }
+    public static function sendEmail(){
+        $user = $_SESSION['user'];
+        $to = $user->getEmail();
+        $pseudo = ucfirst($user->getPseudo());
+        $subject = 'Bienvenue dans mon blog !';
+        $message = <<<EOD
+        <p>Bienvenue $pseudo !<br/><br/>
+        Merci de ton inscription à mon blog<br/></p>
+        <img src="https://media1.tenor.com/images/b0321de1096222775688294decbd823e/tenor.gif?itemid=5183369" alt="Jake's Eyes Sparkling Gif"/>
+EOD;
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+        mail($to, $subject, $message, $headers);
+    }
+    public static function updateUser(){
+        $user = $_SESSION['user'];
+        require('model/UserManager.php');
+        $userManager = new UserManager();
+        $userManager->updateUser($user);
     }
 }
