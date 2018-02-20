2168a968390ced4d5f5886b278ba8a6905ba09ca
esc4pe_re4lity <fiamma.pellicane@gmail.com>
2018-02-20 21:58:25 +0100

    Ajout de la méthode updatePassword

--- model/UserManager.php
+++ model/UserManager.php
@@ -4,7 +4,7 @@ class UserManager
 {
    public function addUser (User $user){
         $db = DBFactory::loadDB();
-        $req=$db->prepare('INSERT INTO users(pseudo, password, email, admin, creationDate) VALUES(:pseudo, :password, :email, false, NOW())');
+        $req=$db->prepare('INSERT INTO users(pseudo, password, email, isAdmin, creationDate) VALUES(:pseudo, :password, :email, false, NOW())');
         $req->execute(array('pseudo'=>$user->getPseudo(),'password'=>$user->getPassword(),'email'=>$user->getEmail()));
     }
     public function login(User $user){
@@ -34,10 +34,17 @@ class UserManager
         $q=$db->query('SELECT * FROM users ORDER BY pseudo');
         return $q;
     }
-    public function updateUser(User $user){
+    public function updateUser(User $updatedUser){
+        $user = $_SESSION['user'];
         $db = DBFactory::loadDB();
-        $req=$db->prepare('UPDATE users SET pseudo=:pseudo, email=:email');
-        $req->execute(array('pseudo'=>$user->getPseudo(),'email'=>$user->getEmail()));
+        $req=$db->prepare('UPDATE users SET pseudo=:newPseudo, email=:newEmail WHERE pseudo=:pseudo');
+        $req->execute(array('newPseudo'=>$updatedUser->getPseudo(),'newEmail'=>$updatedUser->getEmail(),'pseudo'=>$user->getPseudo()));
+    }
+    public function updatePassword(User $updatedUser){
+        $user = $_SESSION['user'];
+        $db = DBFactory::loadDB();
+        $req=$db->prepare('UPDATE users SET password=:password WHERE pseudo='.$user->getPseudo());
+        $req->execute([':password'=>$updatedUser->getPassword()]);
     }
     public function deleteUser(){
         $db = DBFactory::loadDB();
