040bedab77b95a472bbdf982d7acf979b91ab3d1
esc4pe_re4lity <fiamma.pellicane@gmail.com>
2018-03-03 13:58:18 +0100

    Ajout d'un fichier config pour éviter de donner l'accès à la bdd

--- model/DBFactory.php
+++ model/DBFactory.php
@@ -3,8 +3,8 @@ class DBFactory
 {
     public static function loadDB(){
         try{
-            $db = new PDO('mysql:host=localhost;dbname=projet4_test1','root','');
-            //$db = new PDO('mysql:host=db714496514.db.1and1.com;dbname=db714496514','dbo714496514','mitS0uu-6691');
+            $dbConfiguration = require('config.php');
+            $db = new PDO('mysql:host='.$dbConfiguration['db_host'].';dbname='.$dbConfiguration['db_name'],$dbConfiguration['db_user'],$dbConfiguration['db_password']);
         }catch(Exception $e){
             die('Error : '.$e->getMessage());
         }
